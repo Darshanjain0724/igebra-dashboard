809724b417a452c33137e442aaa9530550b43ba5
Darshan Jain <darshanajain0724@gmail.com>
2025-09-17 23:26:47 +0530

    Student database

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b627ac05a2bfbb05"; }
+/******/ 		__webpack_require__.h = function() { return "77cd7ac5bf07574c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
